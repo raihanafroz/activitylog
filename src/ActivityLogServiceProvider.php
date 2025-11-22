2711a6759d0a72a598ec1fe77fd3e032f12d6b07
raihanafroz9 <raihanafroz9@gmail.com>
2025-11-22 12:02:20 +0600

    old file auto delete

--- src/ActivityLogServiceProvider.php
+++ src/ActivityLogServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace RAST\ActivityLog;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
@@ -9,6 +10,8 @@ class ActivityLogServiceProvider extends ServiceProvider
 {
   public function boot()
   {
+    $this->cleanupOldLogs();
+
     if ($this->app->runningInConsole()) {
       $this->publishes([
         __DIR__ . '/../config/activitylog.php' => config_path('activitylog.php'),
@@ -34,10 +37,47 @@ class ActivityLogServiceProvider extends ServiceProvider
   }
 
 
-  protected function registerRoutes()
+  protected function registerRoutes() : void
   {
     Route::middleware('web')
       ->prefix('activity-log')
       ->group(__DIR__ . '/routes/web.php');
   }
+  protected function cleanupOldLogs()
+  {
+    $days = config('activitylog.days', 30);
+
+    Log::debug("Cleaning up old logs older than $days days");
+
+    // Get the log folder dynamically
+    $logPath = config('logging.channels.activity.path');
+
+    Log::debug("Log path: $logPath");
+    $directory = dirname($logPath);
+
+    Log::debug("Directory: $directory");
+
+    if (!is_dir($directory)) {
+      Log::debug("Directory does not exist: $directory");
+      return;
+    }
+    // Extract prefix from file name (activity.log → activity)
+    $filename = basename($logPath);
+    Log::debug("Filename: $filename");
+    $prefix = pathinfo($filename, PATHINFO_FILENAME);
+
+    Log::debug("Prefix: $prefix");
+
+    foreach (glob($directory . '/'.$prefix.'-*.log') as $file) {
+      Log::debug("Checking file: $file");
+      $modified = filemtime($file);
+
+      if ($modified < now()->subDays($days)->timestamp) {
+        @unlink($file);
+      }
+    }
+  }
+
+
+
 }
